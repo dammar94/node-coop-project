9b21b2b9889d71d9ca1777ea016f69e469c70440
MaestroKento <dammar94@gmail.com>
2022-06-05 13:20:44 +0200

    fix path

--- src/app/insert-vacancy/insert-vacancy.component.ts
+++ src/app/insert-vacancy/insert-vacancy.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AppState } from '../state/app.store';
-import * as VacancyActions from 'src\\app\\state\\vacancy\\vacancy.actions';
+import * as VacancyActions from '../state/vacancy/vacancy.actions';
 import { JobType } from '../job-type';
 
 @Component({
